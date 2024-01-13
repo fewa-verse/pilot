dcace93c6c5365cc48dddae4e64d3e5065454c9b
Artem Dolya <artem.dolya@gmail.com>
2024-01-13 17:04:14 -0500

    Fixed Repo build

--- src/Data/FEWA.Data.Abstractions/IRepository.cs
+++ src/Data/FEWA.Data.Abstractions/IRepository.cs
@@ -7,7 +7,7 @@ public interface IRepository
 public interface IRepository<T> : IRepository
 {
     Task<T?> GetByIdAsync(Guid id);
-    Task<IReadOnlyList<T>> GetByAsync();
+    IEnumerable<T> GetByAsync(Func<T, bool>? predicate);
     Task<T> AddAsync(T entity);
     Task UpdateAsync(T entity);
     Task DeleteAsync(T entity);
